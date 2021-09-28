1ad69e090c6cd0bbb3e7c9ed55e5487b586f43f8
Arma-17 <77484963+Arma-17@users.noreply.github.com>
2021-09-28 12:14:04 +0200

    test

---
+++ cmake-build-debug/CMakeFiles/ElaboratoLaboratorio.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ElaboratoLaboratorio.dir/main.cpp.obj"
+  "ElaboratoLaboratorio.exe"
+  "ElaboratoLaboratorio.exe.manifest"
+  "ElaboratoLaboratorio.pdb"
+  "libElaboratoLaboratorio.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ElaboratoLaboratorio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by arman on 28/09/2021.
+//
+
+#include<iostream>
+
+int main(){
+    std::cout<<"Hello Word"<<std::endl;
+}
